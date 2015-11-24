6bf269280bf3544d599c5604f3531e33a5865e02
Avi <avinash.jairam@gmail.com>
2015-11-24 14:15:08 -0500

    Pages converted toold design. Resume.php finished

--- contact.php
+++ contact.php
@@ -1,52 +1,114 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-	<title>Contact | Avinash Jairam</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About | Avinash Jairam</title>
+	<meta charset="utf-8">	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link type="text/css" rel="stylesheet" href="./stylesheets/style1.css"/>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Latest compiled and minified JavaScript -->
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<script type="text/javascript">var switchTo5x=true;</script>         
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 </head>
 
+<body>
+	<nav class="navbar navbar-default">
+	  		<div class="container-fluid">
+	    		<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+	  				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	    				<span class="sr-only">Toggle navigation</span>
+	    				<span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+	  				</button>
+	  				<div class="pushRight">
+	 					<a class="navbar-brand" href="./index.php"><h4>AVINASH JAIRAM | SOFTWARE DEVELOPER</h4></a>
+	 				</div>
+				</div>
 
+	    		<!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      <ul class="nav navbar-nav">
+			        <li><a href="./index.php"><h4>About Me </h4><span class="sr-only">(current)</span></a></li>
+			        <li><a href="./resume.php"><h4>Resume</h4></a></li>
+			        <li><a href="./projects.php"><h4>Projects</h4></a></li>
+			        <li class="active"><a href="./contact.php"><h4>Contact Me</h4></a></li>		        
+			      </ul>
+			     
+			     <!--
+			      <ul class="nav navbar-nav navbar-right">
+			      	<li><a href="https://www.linkedin.com/in/ajairam"><img class="social" src="./images/linkedin.png"/></a></li>
+			        <li><a href="https://github.com/Avinash-Jairam"><img class="social" src="./images/github.png"/></a></li>		
+			      </ul>-->
+			   </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 
-<body data-target="#navbar">
-	<div class="navbar navbar-default">
-		<div class="container">    
-	      <div class="navbar-header">
-	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	            <span class="icon-bar"></span>
-	         </button>
-
-	         <a class="navbar-brand" href="./index.php"><h2>AVINASH JAIRAM - SOFTWARE DEVELOPER</h2></a>
-	      </div>
-
-		    <div class=" collapse navbar-collapse" id="navbar">
-		        <div id="pushDown">
-		          <ul class="navbar-nav nav">
-		            <li><a href="./index.php">About Me</a></li>
-		            <li><a href="./resume.php">Resume</a></li>
-		            <li><a href="./projects.php">Projects</a></li>	            
-		            <li class="active"><a  href="./contact.php">Contact</a></li>
-		          </ul>
-		        </div>
-		    </div>        
-		</div>
+	<div class="container contentContainer">
+			<div class="row">
+				<div class="col-md-12 box" >
+					
+				</div>
+			</div>
 	</div>
 
+	<div class="container contentContainer pushDown" id="footer">
+			<div class="row">
+				<div class="col-md-4 alignLeft" >					
+						<ul>
+							<li><span class=" leftFooter glyphicon glyphicon-map-marker"></span>LOCATED IN BROOKLYN, NY</li>
+							<li><a href="./contact.php"><span class="leftFooter glyphicon glyphicon-envelope"></span>EMAIL ME</a></li>					
+							<li id="resume"><a href="./Resume.pdf"><span class="leftFooter glyphicon glyphicon-download-alt"></span>DOWNLOAD RESUME PDF</a></li>
+						</ul>
+					
+				</div>
+
+				<div class="col-md-3" >
+					
+				</div>
+
+				<div class="col-md-5 omaha" >
+									
+					   <ul >
+				      	<li class="socialFooter"><a href="https://www.linkedin.com/in/ajairam"><img class="social" src="./images/linkedin.png"/></a></li>
+				        <li class="socialFooter"><a href="https://github.com/Avinash-Jairam"><img class="social" src="./images/github.png"/></a></li>		
+				      </ul>	 					
+				</div>
+
+				
+			</div>
+		</div>
+
+		<div class="container contentContainer">
+			<div class="row">
+				<div class="col-md-12 center" >
+					<h6>&copyAvinashJairam.com  <?php echo date("Y") ?> </h6>
+				</div>
+			</div>
+		</div>
+
+             <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69727321-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+	
+
 </body>
-
-
-
-
-<html>
+</html>
