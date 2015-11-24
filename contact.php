56d6d16a5500d7fac2de5da44708f7b69342a47c
Avi <avinash.jairam@gmail.com>
2015-11-24 11:57:11 -0500

    Basic markup of all pages added

---
+++ contact.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Contact | Avinash Jairam</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script type="text/javascript">var switchTo5x=true;</script>         
+</head>
+
+
+
+<body>
+
+
+
+</body>
+
+
+
+
+<html>
