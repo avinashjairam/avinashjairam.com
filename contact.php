8bac22bd170171ba8ce839f966adcf6878b4249c
Avi <avinash.jairam@gmail.com>
2015-11-25 15:48:15 -0500

    Contact Page completed

--- contact.php
+++ contact.php
@@ -1,3 +1,65 @@
+<?php
+// define variables and set to empty values
+$to       = "avinash.jairam@gmail.com";
+$nameErr  = $emailErr = $subjectErr = $messageErr = $captchaErr= "";
+$name     = $email = $subject = $message = "";
+$captcha;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } 
+  else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } 
+  else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+
+  if (empty($_POST["subject"])) {
+    $subjectErr = "Subject is required";
+  } 
+  else {
+    $subject = test_input($_POST["subject"]);
+  }
+
+  if (empty($_POST["message"])) {
+    $messageErr = "Message is required";
+  } else {
+    $message = test_input($_POST["message"]);
+  }
+  if(isset($_POST['g-recaptcha-response'])){
+      $captcha=$_POST['g-recaptcha-response'];
+  }
+  else{
+      $captchaErr="Plese check the captcha form";
+  }
+
+  $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfUoRATAAAAAIt1fJPmWdPv_gNasSwSKgEbi6hv=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+
+  if(empty($nameErr) && empty($emailErr) && empty($subjectErr) && empty($messageErr) && empty($captchaErr) && $response.success==true){
+    $headers="From: ".$name . "via ".$email."\r\n"; 
+    if(mail($to,$subject,$message,$headers)){
+      echo "<script>alert('Message Sent');</script>";
+    }
+  }
+ 
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,6 +78,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 
 </head>
 
@@ -55,8 +118,45 @@
 
 	<div class="container contentContainer">
 			<div class="row">
-				<div class="col-md-12 box" >
-					
+				<div class="col-md-6 box" >
+					<form method="post">
+                       <div class="form-group">                              
+                             <label>Name</label><span class="error">*</span>
+                            <input type="Name" class="form-control" placeholder="Name" name="name" value="<?php echo $_POST['name'];?>"/>
+                      </div>
+            
+	                   <div class="form-group">
+	                           <label>Email</label><span class="error">*</span>
+	                           <input type="Email" class="form-control" placeholder="Email" name="email" value="<?php echo $_POST['email'];?>"/>
+	                    </div>
+
+	                    <div class="form-group">
+	                           <label>Subject</label><span class="error">*</span>
+	                           <input type="Subject" class="form-control" placeholder="Subject" name="subject" value="<?php echo $_POST['subject'];?>"/>
+	                    </div>            
+	                            
+
+		                  <div class="form-group">
+		                           <label>Message</label> <span class="error">*</span>                      
+		                          <textarea class="form-control" name="message" value="<?php echo $_POST['message'];?>" /></textarea>                   
+		                  </div>
+
+		                  <div class="form-group">
+		                    <div class="g-recaptcha" data-sitekey="6LfUoRATAAAAAIt1fJPmWdPv_gNasSwSKgEbi6hv"></div>
+		                  </div>
+
+		                 <div class="form-group">
+		                         <input type="submit" class="btn btn-success" value="Send" name="submit" />
+		                         <input type="submit" class="btn btn-danger" value="Clear" />
+
+		                  </div>
+                        <span class="error"><?php echo " * Field Required";?></span><br><br>
+                        <span class="error"><?php echo $nameErr;?></span><br>
+                        <span class="error"><?php echo $emailErr;?></span><br>
+                        <span class="error"><?php echo $subjectErr;?></span><br>
+                        <span class="error"><?php echo $messageErr;?></span><br>
+                        <span class="error"><?php echo $captchaErr;?></span><br>
+                    </form>
 				</div>
 			</div>
 	</div>
